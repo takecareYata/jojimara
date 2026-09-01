cdf043aa940a45f89ae8ee1c6c0a4dd922a89b37
rlagudwls5518 <rlagudwls5518@naver.com>
2026-09-01 15:42:10 +0900

    feat: 메인 로직 추가

--- work/stm32/main.c
+++ work/stm32/main.c
@@ -1,15 +1,32 @@
 #include "device_driver.h"
 
 #include <stdio.h>
-// static void Sys_Init(int baud) 
-// {
-// 	SCB->CPACR |= (0x3 << 10*2)|(0x3 << 11*2); 
-// 	//Uart2_Init(baud);
-// 	setvbuf(stdout, NULL, _IONBF, 0);
-// }
+static void Sys_Init(int baud) 
+{
+	SCB->CPACR |= (0x3 << 10*2)|(0x3 << 11*2); 
+    Clock_Init();
+	Uart2_Init(baud);
+    Uart1_Init(baud);
+    
+	setvbuf(stdout, NULL, _IONBF, 0);
+}
 
+
+char cmd_buf[64];
+volatile int Uart_Data_In;
 
 void Main(void){
-    //Sys_Init(115200);
-    printf("gigi");
+    Sys_Init(115200);
+    Uart1_RX_Interrupt_Enable(1);
+    printf("test_start\n");
+    while(1){
+       
+        if(Uart_Data_In){
+            printf("before[Jetson -> MCU]: %s\r\n", cmd_buf);
+            app_process_command(UART_ParseCommand(cmd_buf));
+            printf("2\n");
+            Uart_Data_In = 0;
+        }
+    }
 }
+
