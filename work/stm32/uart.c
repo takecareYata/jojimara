f3ab2a7515a7dd12af92b754bebdf0481db00c3d
rlagudwls5518 <rlagudwls5518@naver.com>
2026-09-01 15:36:53 +0900

    feat: uart.c 파일 추가 - uart_init 함수 추가 - send_string, send_char 함수 추가 - RX_interrupt_enable 함수 추가

--- work/stm32/uart.c
+++ work/stm32/uart.c
@@ -1,1 +1,95 @@
 #include "device_driver.h"
+
+
+#define UART_RX_BUFFER_SIZE 64
+volatile bool s_cmd_ready = false;
+static char s_cmd_buf[UART_RX_BUFFER_SIZE];
+
+void Uart2_Init(int baud)
+{
+  double div;
+  unsigned int mant;
+  unsigned int frac;
+
+  Macro_Set_Bit(RCC->AHB1ENR, 0);                   // PA2,3
+  Macro_Set_Bit(RCC->APB1ENR, 17);                   // USART2 ON
+  Macro_Write_Block(GPIOA->MODER, 0xf, 0xa, 4);     // PA2,3 => ALT
+  Macro_Write_Block(GPIOA->AFR[0], 0xff, 0x77, 8);  // PA2,3 => AF07
+  Macro_Write_Block(GPIOA->PUPDR, 0xf, 0x5, 4);     // PA2,3 => Pull-Up  
+
+  volatile unsigned int t = GPIOA->LCKR & 0x7FFF;
+  GPIOA->LCKR = (0x1<<16)|t|(0x3<<2);                // Lock PA2, 3 Configuration
+  GPIOA->LCKR = (0x0<<16)|t|(0x3<<2);
+  GPIOA->LCKR = (0x1<<16)|t|(0x3<<2);
+  t = GPIOA->LCKR;
+
+  div = PCLK1/(16. * baud);
+  mant = (int)div;
+  frac = (int)((div - mant) * 16. + 0.5);
+  mant += frac >> 4;
+  frac &= 0xf;
+
+  USART2->BRR = (mant<<4)|(frac<<0);
+  USART2->CR1 = (1<<13)|(0<<12)|(0<<10)|(1<<3)|(1<<2);
+  USART2->CR2 = 0<<12;
+  USART2->CR3 = 0;
+}
+
+void Uart1_Init(int baud)
+{
+  double div;
+  unsigned int mant;
+  unsigned int frac;
+
+  Macro_Set_Bit(RCC->AHB1ENR, 0);                   // PA9,10
+  Macro_Set_Bit(RCC->APB2ENR, 4);                   // USART1 ON
+  Macro_Write_Block(GPIOA->MODER, 0xf, 0xa, 18);    // PA9,10 => ALT
+  Macro_Write_Block(GPIOA->AFR[1], 0xff, 0x77, 4);  // PA9,10 => AF07
+  Macro_Write_Block(GPIOA->PUPDR, 0xf, 0x5, 18);    // PA9,10 => Pull-Up
+
+  div = PCLK2 / (16. * baud);
+  mant = (int)div;
+  frac = (int)((div - mant) * 16 + 0.5);
+  mant += frac >> 4;
+  frac &= 0xf;
+  USART1->BRR = (mant<<4)|(frac<<0);
+
+  USART1->CR1 = (1<<13)|(0<<12)|(0<<10)|(1<<3)|(1<<2);
+  USART1->CR2 = 0 << 12;
+  USART1->CR3 = 0;
+}
+
+
+
+void UART1_GetCommand(char *out_buf) {
+    if (out_buf != NULL) {
+        strcpy(out_buf, s_cmd_buf);
+    }
+    s_cmd_ready = false;
+}
+
+void UART1_SendChar(char c) {
+    while (!(USART1->SR & USART_SR_TXE));
+    USART1->DR = (uint8_t)c;
+}
+
+void UART1_SendString(const char *str) {
+    while (*str) {
+        UART1_SendChar(*str++);
+    }
+}
+
+void Uart1_RX_Interrupt_Enable(int en)
+{
+  if(en)
+  {
+    Macro_Set_Bit(USART1->CR1, 5);
+    NVIC_ClearPendingIRQ(37);
+    NVIC_EnableIRQ(37);
+  }
+  else
+  {
+    Macro_Clear_Bit(USART1->CR1, 5);
+    NVIC_DisableIRQ(37);
+  }
+}
