47b6d202d1bcb4560919c911f209a4a3b05c88de
rlagudwls5518 <rlagudwls5518@naver.com>
2026-09-03 10:08:07 +0900

    feat: 타이머 1번 딜레이-> 인터럽트로 변경

--- work/stm32/timer.c
+++ work/stm32/timer.c
@@ -1,49 +1,22 @@
 #include "device_driver.h"
 
-// 타이머 1번 딜레이 설정
-#define TIM1_TIC        20                          // usec (1 tick = 20us)
-#define TIM1_FREQ       (1000000. / TIM1_TIC)       // 50,000 Hz (50kHz)
-#define TIM1_1ms_PLS    (TIM1_FREQ / 1000.)         // 1ms당 50 펄스
-#define TIM1_MAX        (0xFFFFu)
 
-
-void TIM1_Delay_Init()
+void TIM1_1ms_Interrupt_Init()
 {
-    Macro_Set_Bit(RCC->APB2ENR, 0); 
+    Macro_Set_Bit(RCC->APB2ENR, 0);
 
-    // Down count (DIR=1, Bit 4), One-pulse mode (OPM=1, Bit 3)
-    TIM1->CR1 = (0x1 << 4) | (0x1 << 3);
+    // 2. 1ms 주기 설정 (SYSCLK 96MHz 기준)
+    // 96MHz / (95 + 1) = 1MHz 카운터 클럭
+    TIM1->PSC = 96 - 1;
+    // 1MHz / 1000 = 1kHz (1ms)
+    TIM1->ARR = 1000 - 1;
 
-    // 20usec tick (50kHz) 설정 
-    TIM1->PSC = (int)(TIMXCLK / TIM1_FREQ + 0.5) - 1;
+    // 3. 인터럽트 설정
+    Macro_Set_Bit(TIM1->DIER, 0);   // Update Interrupt Enable (UIE, Bit 0)
+    Macro_Set_Bit(TIM1->EGR, 0);    // 설정값 반영을 위한 UG 발생
+    Macro_Clear_Bit(TIM1->SR, 0);   // 발생한 플래그 초기화
 
-    Macro_Set_Bit(TIM1->EGR, 0);    // PSC 적용을 위한 UG 발생
-    Macro_Clear_Bit(TIM1->SR, 0);   
-}
-
-/* 딜레이 함수 */
-void TIM1_Delay(int time)
-{
-    if (time <= 0) return;
-
-    unsigned int pls = (unsigned int)(time * TIM1_1ms_PLS);
-    int n = pls / TIM1_MAX; 
-    int m = pls % TIM1_MAX;
-
-    for (int i = 0; i < n; i++) {
-        TIM1->ARR = TIM1_MAX;
-        Macro_Set_Bit(TIM1->EGR, 0);    
-        Macro_Clear_Bit(TIM1->SR, 0);   
-        Macro_Set_Bit(TIM1->CR1, 0);    
-        while (!(TIM1->SR & 0x1));      
-    }
-
-    // 2. 자투리 시간(m) 대기 (m이 0보다 클 때만 실행해야 멈추지 않음)
-    if (m > 0) {
-        TIM1->ARR = m;
-        Macro_Set_Bit(TIM1->EGR, 0);
-        Macro_Clear_Bit(TIM1->SR, 0);
-        Macro_Set_Bit(TIM1->CR1, 0);
-        while (!(TIM1->SR & 0x1));
-    }
+    // 4. NVIC 인터럽트 활성화
+    NVIC_EnableIRQ(TIM1_UP_TIM10_IRQn);
+    NVIC_SetPriority(TIM1_UP_TIM10_IRQn, 2); // 통신 인터럽트보다 우선순위를 낮게 설정 권장
 }
