aaa418a8ecfbd05c92ce22766b8937e3b4485c85
rlagudwls5518 <rlagudwls5518@naver.com>
2026-09-07 17:12:02 +0900

    chore: 하드코딩 제거

--- work/stm32/buzzer_app.c
+++ work/stm32/buzzer_app.c
@@ -1,15 +1,20 @@
 #include "buzzer_app.h"
 #include "buzzer.h"
 
+#define BZ_WARNING_HZ (3000)
+#define BZ_TOGGLE_MS  (250)
+#define BUZZER_CHANNEL        3
+#define BUZZER_TIM_CLK_HZ     1000000U
+
 static Buzzer_t buzzer;
 
-volatile unsigned int warning_tick = 0;
+volatile static unsigned int warning_tick = 0;
 volatile static int warning_state = 0;
-volatile int is_bz_running_tick = 0;
+volatile static int is_bz_running_tick = 0;
 
 void app_buzzer_init(){
     buzzer_hw_init();
-    buzzer_time_init(&buzzer, TIM3, 3, 1000000);
+    buzzer_time_init(&buzzer, TIM3, BUZZER_CHANNEL, BUZZER_TIM_CLK_HZ);
 }
 
 // 소리만 잠시 끄는 함수 (PWM OFF)
@@ -22,8 +27,8 @@ void app_buzzer_mute()
 void app_start_buzzer()
 {
     warning_tick = 0;
-    buzzer_insert_hz(&buzzer, 3000);
-    is_bz_running_tick =1;
+    buzzer_insert_hz(&buzzer, BZ_WARNING_HZ);
+    is_bz_running_tick = 1;
 }
 
 // 경고음 완전히 정지 (명령 수신 시 1회 호출)
@@ -37,7 +42,7 @@ void app_buzzer_interrupt(){
     if(is_bz_running_tick){
         warning_tick++;
 
-        if (warning_tick >= 250) // 250ms 주기마다 On/Off 토글
+        if (warning_tick >= BZ_TOGGLE_MS) // 250ms 주기마다 On/Off 토글
         {
             warning_tick = 0;    
             warning_state ^= 1; 
@@ -50,7 +55,7 @@ void app_buzzer_interrupt(){
             else
             {
                 // 소리 켬
-                buzzer_insert_hz(&buzzer, 3000); // 삐- 삐- 단속음  
+                buzzer_insert_hz(&buzzer, BZ_WARNING_HZ); // 삐- 삐- 단속음  
             }
         }
     }
